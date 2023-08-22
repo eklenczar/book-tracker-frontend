18a4e97fa88beee1994e9af26e21fc48f4ff4b61
Elizabeth Klenczar <eklenczar@gmail.com>
2023-08-22 09:48:54 -0400

    Add NewBookForm styling

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,32 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BookCard from "./BookCard";
-import "./BookContainer.css"
-
+import "./BookContainer.css";
 
 function BookContainer({ books, setBooks }) {
-  
-
   function handleDeleteBook(deletedBook) {
-    const updatedBook = books.filter(
-      (book) => book.id !== deletedBook.id
-    );
+    const updatedBook = books.filter((book) => book.id !== deletedBook.id);
     setBooks(updatedBook);
   }
-  
-  function bookRenderer (book) {
-    return <BookCard key={book.id} book={book} onBookDelete={handleDeleteBook}/>
+
+  function bookRenderer(book) {
+    return (
+      <BookCard key={book.id} book={book} onBookDelete={handleDeleteBook} />
+    );
   }
   const renderBooks = books.map(bookRenderer);
 
   return (
-  <div className="book-container">
-    <Link to={"/books/new"} className="new-book-link">
-      Add New Book
-    </Link>
-    <div>{renderBooks}</div>
-  </div>
-  )
+    <div>
+      <div className="new-book-button">
+        <Link to={"/books/new"}>
+          <button>Add New Book</button>
+        </Link>
+      </div>
+      <div>{renderBooks}</div>
+    </div>
+  );
 }
 
 export default BookContainer;
--- src/components/NewBookForm.js
+++ src/components/NewBookForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./NewBookForm.css"
 
 function NewBookForm({ onAddBook }) {
   const [title, setTitle] = useState("");
@@ -38,38 +39,51 @@ function NewBookForm({ onAddBook }) {
   }
 
   return (
-    <div>
-      <form>
-        <label>Title</label>
-        <br />
-        <input name="title" value={title} onChange={handleTitle} />
-        <br />
-        <label>Author</label>
-        <br />
-        <input name="author" value={author} onChange={handleAuthor} />
-        <br />
-        <label>Genre</label>
-        <br />
-        <input name="genre" value={genre} onChange={handleGenre} />
-        <br />
-        <label>Image</label>
-        <br />
-        <input name="image" value={image} onChange={handleImage} />
-        <br />
-        <label>Description</label>
-        <br />
-        <input name="description" value={description} onChange={handleDesc} />
-        <br />
-        <button onClick={handleNewBookSubmit}>Submit</button>
-      </form>
-      {errors.length > 0 && (
-        <ul style={{ color: "red" }}>
-          {errors.map((error) => (
-            <li key={error}>{error}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div className="new-book-container">
+        <div className="new-book-center">
+          <form>
+            <label>Title</label>
+            <br />
+            <input name="title" value={title} onChange={handleTitle} />
+            <br />
+            <label>Author</label>
+            <br />
+            <input name="author" value={author} onChange={handleAuthor} />
+            <br />
+            <label>Genre</label>
+            <br />
+            <input name="genre" value={genre} onChange={handleGenre} />
+            <br />
+            <label>Image</label>
+            <br />
+            <input name="image" value={image} onChange={handleImage} />
+            <br />
+            <label>Description</label>
+            <br />
+            <input
+              name="description"
+              value={description}
+              onChange={handleDesc}
+            />
+            <br />
+            <br />
+            <div className="new-book-submit-button">
+            <button onClick={handleNewBookSubmit}>Submit</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div>
+        {errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 }
 
